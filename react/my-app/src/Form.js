541fb9be4278b5e59b3c0dafe308fe553ae4f2f7
marymafa <marymafa80@gmail.com>
2018-03-14 14:25:29 +0200

    made changes

--- react/my-app/src/Form.js
+++ react/my-app/src/Form.js
@@ -1,73 +1,83 @@
 import React from "react"
-import confirmationPage from "./confirmationPage"
 
-export default class Form extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        Lastname: "",
-        EmailAddress: "",
-        message: ""
-      }; this.handleChange = this.handleChange.bind(this);//his binding is necessary to make `this` work in the callback
-      this.handleSubmit = this.handleSubmit.bind(this);//This syntax ensures `this` is bound within handleSubmit
 
-    }
-    
-    onChange = (e) => {
-      const state = this.state;
-      state[e.target.name] = e.target.value;
-      this.setState(state);
+class Form extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: ' ',
+      email: ' ',
+      contactNo: '',
+    };
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
   }
 
   onSubmit = (e) => {
-      e.preventDefault();
-      const { name, email, message } = this.state;
+    e.preventDefault();
+    alert('thanks for submitting: ' + this.state.email);
+    const form = {
+      name: this.state.name,
+      email: this.state.email,
+      contactNo: this.state.contactNo,
+      Gender: this.state.Gender
 
+    }
+
+    e.push(form);
+    debugger;
+    this.setState({
+      name: '',
+      email: '',
+      contactNo: '',
+      Gender: '',
+
+    })
   }
-    handleChange(event) {//receives an javascript event
-      this.setState({value: event.target.value});
-    }
-    handleSubmit(event) {
-      alert('thanks for submitting: ' + this.state.value);
-      event.preventDefault();
-      
-    }
-  
-    render() {
-      const { name, email, message } = this.state;
-      return (
-  
-        <form  onSubmit={this.handleSubmit}>
-  
-          React form:<br />
+
+  render() {
+    return (
+      <div>
+        <form>
+          React Form:<br />
           <label>
-            Firstname:
-      <input type="text" name="firstname" placeholder="firstname"  onChange={this.handleChange}/>
-          </label><br />
-          <label>
-            Lastname:
-      <input type="text" name="lastname" placeholder="lastname"  onChange={this.handleChange}/>
+            Name:
+                  <input
+              name='name'
+              value={this.state.name}
+              onChange={e => this.handleChange(e)} />
           </label><br />
           Gender:
-  
+
   <select>
-            <option selected value="select"  onChange={this.handleChange}>select</option>
+            <option selected value="select" onChange={this.handleChange}>select</option>
             <option value="both">Both</option>
             <option value="male">male</option>
             <option value="female">Female</option>
           </select><br />
           <label>
-            ContactNo:
-      <input type="text" name="contactno" placeholder="contactno"   onChange={this.handleChange}/>
+            Email:
+                  <input
+              name='email'
+              value={this.state.email}
+              onChange={e => this.handleChange(e)} />
           </label><br />
           <label>
-            EmailAddress:
-      <input type="text" name="emailaddress" placeholder="emailaddress"  onChange={this.handleChange}/>
+            contactNo:
+                  <input
+              name='contactno'
+              value={this.state.contactno}
+              onChange={e => this.handleChange(e)} />
           </label>
-          <input type="submit" value="Submit" />
-  
+          <button onClick={(e) => this.onSubmit(e)}>Send</button>
         </form>
-  
-      );
-    }
+      </div>
+    );
   }
+}
+
+export default Form;
--- react/my-app/src/index.js
+++ react/my-app/src/index.js
@@ -1,9 +1,11 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Form from "./Form"
-import confirmationPage from  "./confirmationPage"
+import Form from "./Form";
 import './index.css';
+
+
+
 
 // class Square extends React.Component {
 //   constructor(props) {
@@ -23,11 +25,6 @@ import './index.css';
 //   }
 
 // }
-
-
-// ReactDOM.render(<Square />, document.getElementById("root"));https://jsfiddle.net/69z2wepo/130095/
-
-
 
 ReactDOM.render(<Form />, document.getElementById("root"))
 
