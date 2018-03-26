95f75779434cccdf7a35ebd9ed3cbffada0e71dc
marymafa <marymafa80@gmail.com>
2018-03-26 12:25:41 +0200

    added some piece of code

---
+++ react/my-app/src/markdown.js
@@ -0,0 +1,13 @@
+
+import React from  "react"
+
+class markdown extends React.Component{
+    
+    render(){
+        return(
+            <div>
+                
+            </div>
+        )
+    }
+}
