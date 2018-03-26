8f568cb82bab2e78d75f6a7e9c55fbe11b6c4d3c
marymafa <marymafa80@gmail.com>
2018-03-26 12:27:13 +0200

    added  render function

--- react/my-app/src/markdown.js
+++ react/my-app/src/markdown.js
@@ -5,9 +5,21 @@ class markdown extends React.Component{
     
     render(){
         return(
-            <div>
-                
-            </div>
+            <div className="container-fluid">
+			<div className="row">
+				<h1 className="text-center">
+					ReactJS Markdown Editor
+				</h1>
+				<div className="col-xs-12 col-sm-6">
+					<h3>Markdown</h3>
+					<textarea id="markdown" className="markdown"></textarea>
+				</div>
+				<div className="col-xs-12 col-sm-6">
+					<h3>Preview</h3>
+					<div id="preview"></div>
+				</div>
+			</div>
+		</div>
         )
     }
 }
